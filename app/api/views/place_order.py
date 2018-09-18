b510bf282ce379ab7dcdaaff2022472d0b5ab134
Gfreedoms <freedomgemmar1@gmail.com>
2018-09-18 15:39:58 +0300

    redifine routes[#160282859]

--- app/api/views/place_order.py
+++ app/api/views/place_order.py
@@ -1,11 +1,14 @@
-from flask import Blueprint, request, jsonify, json
-from app.api.models import Order, CustomerOrders, FeedbackResponse
+"""
+Module to handle placementof order
+"""
+from flask import Blueprint, request
+from app.api.models import Order, FeedbackResponse
 from app.api.views.customer_orders import customer_orders
 
-fastfood_place_order = Blueprint('place_order', __name__)
+FFF_POST = Blueprint('place_order', __name__)
 
 
-@fastfood_place_order.route('/api/v1/orders', methods=['POST'])
+@FFF_POST.route('/api/v1/orders', methods=['POST'])
 def place_order():
     """
      method to create new order and apend to the existing list
@@ -17,10 +20,9 @@ def place_order():
         if not data.get('order_description'):
             return FeedbackResponse.display("set description please", 406)
         if not data.get('size'):
-            return FeedbackResponse.display("set size please ", 406),
+            return FeedbackResponse.display("set size please ", 406)
         if not data.get('order_price'):
-            return FeedbackResponse.display("set price please", 406)        
-
+            return FeedbackResponse.display("set price please", 406)
         order_number = data.get('order_number')
         order_description = data.get('order_description')
         order_price = data.get('order_price')
@@ -30,4 +32,8 @@ def place_order():
                           order_price, size)
 
         customer_orders.place_order(new_order)
-        return FeedbackResponse.display("order number "+ order_number +" successfully placed", 200)
+        return FeedbackResponse.display(
+            "order number " +
+            order_number +
+            " successfully placed",
+            200)
